4eecfa8074b8c658539c544816fe7bd66a39e3c6
Naresh Sharma <sharmanaresh272@gmail.com>
2022-12-15 22:30:31 +0530

    .

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -97,7 +97,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                               return null;
                             },
                           ),
-                          SizedBox(height: 15),
+                          const SizedBox(height: 15),
                           CustomTextbox(
                             Icons.lock_outline,
                             'Password',
