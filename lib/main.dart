792d3fdf1f091102678f3488ed6bf18db3d1a0d8
Dhruval Patadiya <sonidhruval2@gmail.com>
2022-12-21 14:31:20 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 import 'router.dart';
 import './screens/home_screen.dart';
 
@@ -9,7 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp());
